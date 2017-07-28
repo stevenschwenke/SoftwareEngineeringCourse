fce7bff1426a48e85fff2812970314b766ab50c9
Steven Schwenke <steven@stevenschwenke.de>
2017-07-28 08:04:15 +0200

    Add markdown file for chapter Design Patterns

---
+++ src/main/java/designPatterns/designPatterns.md
@@ -0,0 +1,5 @@
+# Design Patterns
+This chapter shows design patterns written in Java.
+
+# Sources
+- Java Magazine November / December 2016 "JUnit 5 Arrives", "Implementing Design Patterns with Lambdas" by Raoul-Gabriel Urma, Mario Fusco and Alan Mycroft
--- src/main/java/designPatterns/observer/Observer.java
+++ src/main/java/designPatterns/observer/Observer.java
@@ -3,6 +3,4 @@ package designPatterns.observer;
 public interface Observer {
 
     void notify(String tweet);
-
-    // TODO Quelle nennen: Java Magazine November / December 2016 "JUnit 5 Arrives", "Implementing Design Patterns with Lambdas" by Raoul-Gabriel Urma, Mario Fusco and Alan Mycroft
 }
